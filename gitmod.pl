77b02a5ec66f94928e9b7bb47ac9ecb10f713929
David <david@deadpansincerity.com>
2010-06-17 15:30:46 +0100

    First working version of script. Likely to be buggy as hell :)

--- gitmod.pl
+++ gitmod.pl
@@ -3,7 +3,31 @@
 use strict;
 use warnings;
 
-my $status;
-$status = `git status`;
-print $status =~ /#\h+modified:\h+(.*)/g;
-print "\n";
+sub is_modechange {
+    my $file = shift(@_);
+    my $diff = `git diff $file`;
+    my $lines = split(/\n/, $diff);
+    my $numlines = length($lines);
+    if ($numlines != 1){
+        return 0;
+    }
+    return 1;
+}
+
+my @commits = ();
+my $status = `git status`;
+my @lines = split /\n/, $status;
+my @mod = grep(m/#\h+modified:(\h+.*)/g, @lines);
+foreach my $file (@mod){
+    my @words = split(/ /, $file);
+    my $f = $words[-1];
+    my $modechange = is_modechange($f);
+    if ($modechange == 1){
+        push(@commits, $f);
+    }
+}
+
+if (length(@commits) > 0){
+    print "we have commits!\n";
+    exec("git commit @commits -m 'Auto commit #perl'");
+}
